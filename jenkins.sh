13a2004ce6820336cbc8bf4d496cef2e8e3aa6fe
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-19 22:44:37 +0000

    init

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+k3d cluster create mycluster --agents 3 -p 30000-32767:30000-32767@server[0]
+source <(k3d completion bash)
+source <(kubectl completion bash)
+kubectl apply -f jenkins.namespace.yaml
+kubectl apply -f jenkins.helm.yaml
+kubectl get pods -n jenkins
+POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
+echo username: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
+echo password: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
