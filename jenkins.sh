4301dcf662ddce1af60b69319b6323125859b6c9
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-19 22:51:02 +0000

    updates script

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ source <(k3d completion bash)
 source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
 kubectl apply -f jenkins.helm.yaml
-kubectl get pods -n jenkins
 POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
+echo "Jenkins credentials"
 echo username: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
 echo password: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
