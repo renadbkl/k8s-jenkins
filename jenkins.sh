00eba3f7077e10704b0a87a6f1319fd2fd5075b3
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-20 08:47:10 +0300

    Added port

--- jenkins.sh
+++ jenkins.sh
@@ -12,3 +12,4 @@ POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs
 echo "Jenkins credentials"
 echo username: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
 echo password: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
+echo port: $(kubectl get service jenkins -n jenkins -o json | jq '.spec.ports[0].nodePort')
