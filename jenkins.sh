2f3e5180e770a89322e319d626db4243ae213e53
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-24 05:29:04 +0000

    separated query and added docker socket volume

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-k3d cluster create mycluster --agents 3 -p 30000-32767:30000-32767@server[0]
+k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
 source <(k3d completion bash)
 source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
@@ -8,8 +8,4 @@ WAIT=60
 echo "Sleeping for $WAIT"
 sleep $WAIT
 echo "Making progress"
-POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
-echo "Jenkins credentials"
-echo username: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
-echo password: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
-echo port: $(kubectl get service jenkins -n jenkins -o json | jq '.spec.ports[0].nodePort')
+. query.sh
