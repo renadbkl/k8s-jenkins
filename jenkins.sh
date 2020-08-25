a9a22d6569b9fa2ee8d44772a321e43a737f88ff
Amerah Almutairi <66031162+amerahsultan1@users.noreply.github.com>
2020-08-25 17:20:49 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
+k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] -p 80:80@loadbalancer
 source <(k3d completion bash)
 source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
 kubectl apply -f jenkins.helm.yaml
+kubectl create -n jenkins clusterrolebinding jenkins-account --clusterrole=cluster-admin --serviceaccount=jenkins:jenkins
 WAIT=60
 echo "Sleeping for $WAIT"
 sleep $WAIT
